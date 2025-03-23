3ca810b2190609b7ecaebc08fd7d433295dda705
twk56 <thanawatklomkliew@gmail.com>
2025-03-23 21:20:29 +0700

    Update logout.tsx

--- src/components/login/logout.tsx
+++ src/components/login/logout.tsx
@@ -5,15 +5,12 @@ const Logout: React.FC = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // ลบ token และข้อมูลผู้ใช้จาก localStorage
     localStorage.removeItem('token');
     localStorage.removeItem('username');
-
-    // เปลี่ยนเส้นทางไปยังหน้า login
     navigate('/login');
   }, [navigate]);
 
-  return null; // ไม่จำเป็นต้องแสดงอะไรในหน้านี้
+  return null;
 };
 
 export default Logout;
