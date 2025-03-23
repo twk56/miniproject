2b5b15f8af918c80e578e39f48cb541562610081
twk56 <thanawatklomkliew@gmail.com>
2025-03-23 21:16:47 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Import styles from Tailwind CSS
-import App from './App'; // Import the main App component
+import './index.css';
+import App from './App';
 
-// Render the App component to the root div in your HTML
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
